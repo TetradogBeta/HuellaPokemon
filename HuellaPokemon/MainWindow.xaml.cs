71d0421cad2f980a48ac21a4bc344d92d9535550
tetraadog <tetradog@gmail.com>
2017-01-11 21:30:10 +0100

    subir datos

---
+++ HuellaPokemon/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PokemonGBAFrameWork; 
+using Microsoft.Win32;
+
+namespace HuellaPokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            OpenFileDialog opnFile;
+            RomData romData;
+            InitializeComponent();
+            opnFile = new OpenFileDialog();
+            opnFile.Filter = "GBA|*.gba";
+            if (opnFile.ShowDialog().GetValueOrDefault())
+            {
+                romData = RomData.GetRomData(new RomGBA(new System.IO.FileInfo(opnFile.FileName)));
+                for (int i = 0; i < romData.Pokedex.Count; i++)
+                    ungPokemon.Children.Add(new PokemonYHuella(romData.Pokedex[i]));
+            }
+            else this.Close();
+        }
+    }
+}
---
+++ HuellaPokemon/pokemonYHuella.xaml.cs
@@ -0,0 +1,36 @@
+﻿using PokemonGBAFrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gabriel.Cat.Extension;
+namespace HuellaPokemon
+{
+    /// <summary>
+    /// Interaction logic for pokemonYHuella.xaml
+    /// </summary>
+    public partial class PokemonYHuella : UserControl
+    {
+        public PokemonYHuella()
+        {
+            InitializeComponent();
+        }
+        public PokemonYHuella(Pokemon pkm):this()
+        {
+            imgPokemon.SetImage(pkm.Sprites.GetImagenFrontal());
+            if(pkm.Huella!=null)
+               imgHuella.SetImage(pkm.Huella.Imagen);
+
+        }
+    }
+}
